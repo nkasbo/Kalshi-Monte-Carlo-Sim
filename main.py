a5600dea0e476c79bc7fe67bd82517de18a3c437
Herschelle Bumrah <bumrahherschelle@gmail.com>
2026-01-07 02:22:36 -0500

    Requests going through and json objects acquired. Next steps are parse and clean data for simulation

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from src.kalshi.api.client import KalshiClient
+
 def main():
-    print("Hello from kalshi-monte-carlo-sim!")
+    print("Hello from kalshi-monte-carlo-simulator!")
+    Ticker = input("Please enter the Market Ticker: ")
+    run1 = KalshiClient()
+    datadump = run1.get_event(Ticker)
+    print(datadump)
 
 
 if __name__ == "__main__":
